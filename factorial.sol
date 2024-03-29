2355357460c392c4a71c20ecda829f0385fe70d1
godavarthikowsik <110228850+godavarthikowsik@users.noreply.github.com>
2024-03-29 12:01:00 +0530

    Update factorial.sol

--- factorial.sol
+++ factorial.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
 
+contract Factorial
+{
+    function fact(uint256 n) public pure returns(uint256)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        uint256 res=1;
+        for(uint i=1;i<=n;i++)
+        {
+            res = res*i;
+        }
+        return res;
+    } 
+}
